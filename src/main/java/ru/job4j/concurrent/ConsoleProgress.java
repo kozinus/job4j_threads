2582ab309c054df957468dcd146ecdd85fd63098
kozinus <kkozin2403@gmail.com>
2023-10-05 14:00:10 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int index = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(500);
+                System.out.print("\r load: " + process[index]);
+                index = (index + 1) % process.length;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
