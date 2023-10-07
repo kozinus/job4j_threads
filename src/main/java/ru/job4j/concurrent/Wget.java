3fe460aa3cf4b4730bd400ae9955d1557e9f2823
kozinus <kkozin2403@gmail.com>
2023-10-07 20:33:02 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,21 +1,63 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i < 101; i++) {
-                            Thread.sleep(1000);
-                            System.out.print("\r Loading: " + i + "%");
-                        }
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        var startAt = System.currentTimeMillis();
+        var file = new File("tmp.xml");
+        try (var in = new URL(url).openStream();
+             var out = new FileOutputStream(file)) {
+            System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
+            var dataBuffer = new byte[512];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                var downloadAt = System.nanoTime();
+                out.write(dataBuffer, 0, bytesRead);
+                long downloadTime = System.nanoTime() - downloadAt;
+                System.out.println("Read 512 bytes : " + downloadTime + " nano.");
+                long sleepTime = (long) (Math.pow(10, 6) * 512 / downloadTime / speed);
+                try {
+                    if (sleepTime > 0) {
+                        Thread.sleep(sleepTime);
+                        System.out.println("Pause: " + sleepTime + "ms.");
                     }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
-                );
-        thread.start();
-        System.out.println("Main");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        if (speed <= 0) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            new URL(url).toURI();
+        } catch (MalformedURLException | URISyntaxException e) {
+             throw new IllegalArgumentException();
+        }
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
-
