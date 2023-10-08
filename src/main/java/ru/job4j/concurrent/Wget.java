c8b8c11a820cae1e3de8fd2bdb6bf756ae198995
kozinus <kkozin2403@gmail.com>
2023-10-08 12:05:23 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -10,35 +10,43 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         var startAt = System.currentTimeMillis();
-        var file = new File("tmp.xml");
+        var file = new File(fileName);
         try (var in = new URL(url).openStream();
              var out = new FileOutputStream(file)) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
-            var dataBuffer = new byte[512];
+            var dataBuffer = new byte[128];
             int bytesRead;
+            int totalBytesRead = 0;
+            int downloadOffset = 1000000;
+            var downloadAt = System.nanoTime();
             while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.nanoTime();
                 out.write(dataBuffer, 0, bytesRead);
+                totalBytesRead += bytesRead;
                 long downloadTime = System.nanoTime() - downloadAt;
-                System.out.println("Read 512 bytes : " + downloadTime + " nano.");
-                long sleepTime = (long) (Math.pow(10, 6) * 512 / downloadTime / speed);
-                try {
-                    if (sleepTime > 0) {
-                        Thread.sleep(sleepTime);
-                        System.out.println("Pause: " + sleepTime + "ms.");
+                System.out.println("Read 128 bytes : " + downloadTime + " nano.");
+                downloadOffset -= downloadTime;
+                if (totalBytesRead > speed && downloadOffset > 0) {
+                    try {
+                        System.out.println("Pause: " + downloadOffset + " nano.");
+                        Thread.sleep(0, downloadOffset);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
                     }
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                    totalBytesRead = 0;
+                    downloadOffset = 1000000;
                 }
+                downloadAt = System.nanoTime();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,17 +54,27 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        if (speed <= 0) {
+        if (args.length != 3) {
             throw new IllegalArgumentException();
         }
+
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        String fileName = args[2];
+
         try {
             new URL(url).toURI();
         } catch (MalformedURLException | URISyntaxException e) {
-             throw new IllegalArgumentException();
+            throw new IllegalArgumentException();
         }
-        Thread wget = new Thread(new Wget(url, speed));
+        if (speed <= 0) {
+            throw new IllegalArgumentException();
+        }
+        if ("".equals(fileName) || !fileName.contains(".")
+                || fileName.startsWith(".") || fileName.endsWith(".")) {
+            throw new IllegalArgumentException();
+        }
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
     }
