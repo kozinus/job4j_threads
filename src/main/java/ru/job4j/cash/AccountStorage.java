81a89762174daee122e1142ab95c7214f5c61c04
kozinus <kkozin2403@gmail.com>
2023-10-16 15:29:55 +0300

    3. Денежные переводы AccountStorage [#1104]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -6,48 +6,30 @@ import java.util.Optional;
 public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
-    public boolean add(Account account) {
-        synchronized (accounts) {
-            boolean flag = !accounts.containsKey(account.id());
-            if (flag) {
-                accounts.put(account.id(), account);
-            }
-            return flag;
-        }
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
     }
 
-    public boolean update(Account account) {
-        synchronized (accounts) {
-            boolean flag = accounts.containsKey(account.id());
-            if (flag) {
-                accounts.put(account.id(), account);
-            }
-            return flag;
-        }
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
     }
 
-    public void delete(int id) {
-        synchronized (accounts) {
-            accounts.remove(id);
-        }
+    public synchronized void delete(int id) {
+        accounts.remove(id);
     }
 
-    public Optional<Account> getById(int id) {
-        synchronized (accounts) {
-            return Optional.ofNullable(accounts.get(id));
-        }
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
     }
 
-    public boolean transfer(int fromId, int toId, int amount) {
-        synchronized (accounts) {
-            Optional<Account> from = getById(fromId);
-            Optional<Account> to = getById(toId);
-            boolean flag = from.isPresent() && to.isPresent() && (from.get().amount() - amount) >= 0;
-            if (flag) {
-                update(new Account(from.get().id(), from.get().amount() - amount));
-                update(new Account(to.get().id(), to.get().amount() + amount));
-            }
-            return flag;
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        Optional<Account> from = getById(fromId);
+        Optional<Account> to = getById(toId);
+        boolean flag = from.isPresent() && to.isPresent() && (from.get().amount() - amount) >= 0;
+        if (flag) {
+            update(new Account(from.get().id(), from.get().amount() - amount));
+            update(new Account(to.get().id(), to.get().amount() + amount));
         }
+        return flag;
     }
 }
