9d83f4f2da64fa3d17b142a00e35ca3a26348a1a
kozinus <kkozin2403@gmail.com>
2023-10-17 14:07:07 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.wait;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleBlockingQueueTest {
+    @Test
+    public void test() throws InterruptedException {
+        var queue = new SimpleBlockingQueue<Integer>(10);
+
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 20; i++) {
+                try {
+                    queue.offer(i);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }, "producer");
+        Thread consumer = new Thread(() -> {
+            for (int i = 0; i < 17; i++) {
+                try {
+                    queue.poll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+
+            }
+        }, "consumer");
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < 3; i++) {
+            set.add(queue.poll());
+        }
+        System.out.println(set);
+        assertThat(set).hasSize(2).containsAll(Set.of(18, 19));
+    }
+}
