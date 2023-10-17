1c690ea090272dd926d458fabb834dd63ee18c6a
kozinus <kkozin2403@gmail.com>
2023-10-17 14:51:08 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/wait/ParallelSearch.java
@@ -0,0 +1,31 @@
+package ru.job4j.wait;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }, "1");
+        consumer.start();
+        new Thread(
+                () -> {
+                    try {
+                        for (int index = 0; index != 3; index++) {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    consumer.interrupt();
+                }, "2").start();
+    }
+}
